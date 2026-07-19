53a76550d03d9cb80d7adf9b8028e815e696813a
LISA873-irina <sunjidajahan873@gmail.com>
2026-07-19 23:50:21 +0600

    Update main.c to use course result module

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-#include "modules/course.h"
+#include "course.h"
+#include "courseResult.h"
 
 int main()
 {
-    printf("In IUT, CGPA Matters!\n");
+    Course courses[3];
+    CourseResult results[3];
+    int n_courses = 0;
 
-    Course course1 = createCourse("CSE4202", "Structured Programming II", 3.0);
-    viewCourse(course1);
+    courses[n_courses++] = createCourse("CSE4107", "Structured Programming I", 3.0);
+    courses[n_courses++] = createCourse("CSE4108", "Structured Programming I Lab", 1.5);
+    courses[n_courses++] = createCourse("CSE4203", "Discrete Mathematics", 3.0);
+
+    for (int i = 0; i < n_courses; i++)
+    {
+        double marks;
+        printf("Marks for %s: ", courses[i].name);
+        scanf("%lf", &marks);
+        results[i] = createCourseResult(&courses[i], marks);
+    }
+
+    for (int i = 0; i < n_courses; i++)
+    {
+        viewCourseResult(results[i]);
+    }
 
     return 0;
 }
