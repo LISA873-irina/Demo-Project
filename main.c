661ec873ff8e0284b7503ff852c8e7d82dc33b8b
LISA873-irina <sunjidajahan873@gmail.com>
2026-07-19 23:01:12 +0600

    Update main.c to use course module

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <stdio.h> 
-int main() 
-{ 
-printf("In IUT, CGPA Matters!\n"); 
-return 0; 
-}
+#include <stdio.h>
+#include "modules/course.h"
 
+int main()
+{
+    printf("In IUT, CGPA Matters!\n");
+
+    Course course1 = createCourse("CSE4202", "Structured Programming II", 3.0);
+    viewCourse(course1);
+
+    return 0;
+}
--- make.ps1
+++ make.ps1
@@ -1,1 +1,1 @@
-gcc main.c -o calculator.exe
+gcc -Imodules main.c modules/course.c -o calculator.exe
