dde5265b1bf9748abbbd6629ecda2459e4ebe1fb
LISA873-irina <sunjidajahan873@gmail.com>
2026-07-20 00:09:02 +0600

    Implement incomplete course result support

--- modules/courseResult.c
+++ modules/courseResult.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include "courseResult.h"
 
-CourseResult createCourseResult(Course *course, double marks)
+CourseResult createCompletedCourseResult(Course *course, double marks)
 {
     CourseResult result;
     result.course = course;
     result.marks = marks;
+    result.completed = 1;
+
+    return result;
+}
+
+CourseResult createIncompleteCourseResult(Course *course)
+{
+    CourseResult result;
+    result.course = course;
+    result.marks = 0.0;
+    result.completed = 0;
 
     return result;
 }
@@ -15,5 +26,9 @@ void viewCourseResult(CourseResult result)
     printf("Code: %s\n", result.course->code);
     printf("Name: %s\n", result.course->name);
     printf("Credit: %.1f\n", result.course->credit);
+    if (!result.completed)
+    {
+        printf("Incomplete\n");
+    }
     printf("Marks: %.2f\n", result.marks);
 }
